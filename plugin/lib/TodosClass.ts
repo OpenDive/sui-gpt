6f58a9c7e6c2d67635dd0410151103c0f5303d53
kPatch <irvsteve@gmail.com>
2023-06-04 07:39:15 +0900

    Add support for querying Sui Balances

--- plugin/lib/TodosClass.ts
+++ plugin/lib/TodosClass.ts
@@ -1,3 +1,6 @@
+import { JsonRpcProvider, devnetConnection, mainnetConnection } from '@mysten/sui.js';
+
+
 interface Todo {
   [key: string]: string
 }
@@ -14,6 +17,21 @@ class TodosClass {
   deleteTodo(todo:Todo):void {
     const index = this.todos.findIndex(item => item.todo === todo.todo)
     this.todos.splice(index, 1)
+  }
+
+ async getBalance():Promise<string> {
+  const provider = new JsonRpcProvider(mainnetConnection);
+  // If coin type is not specified, it defaults to 0x2::sui::SUI
+  const coinBalance = await provider.getBalance({
+    owner: '0x9ccae709d2271c28bbf695293243315ef7342e8e2cc927fa651ad3fbfc7bda9f',
+    // coinType:
+    //   // '0x65b0553a591d7b13376e03a408e112c706dc0909a79080c810b93b06f922c458::usdc::USDC',
+    //   '0x2::sui::SUI',
+  });
+
+  console.log(JSON.stringify(coinBalance));
+  console.log("USDC COINS: " + coinBalance.totalBalance);
+  return coinBalance.totalBalance;
   }
 }
 
